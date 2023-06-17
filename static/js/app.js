119945b16812f4b7f044b4522a29b407ca98e91f
meinhardtmr <meinhardtmr@netscape.net>
2023-06-17 17:03:55 -0400

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -5,8 +5,7 @@ let metadata = [];
 let logging = true;
 
 function populateDemoInfo(id){
-  //logging?console.log('populateDemoInfo: ', id, metadata.find(o => o.id == id).id): '' ;
-  console.log('populateDemoInfo: ', id, metadata.find(o => o.id == id).id);
+  logging ? console.log('populateDemoInfo: ', id, metadata.find(o => o.id == id).id) : null ;
   
   let str = `<p>id:        ${metadata.find(o => o.id == id).id}        </p>
              <p>ethnicity: ${metadata.find(o => o.id == id).ethnicity} </p>
@@ -21,8 +20,7 @@ function populateDemoInfo(id){
 }
 
 function createHBarChart(id){
-  //logging?console.log('createHBarChart: ', id, samples.find(o => o.id == id).id): '' ;
-  console.log('createHBarChart: ', id, samples.find(o => o.id == id).id);
+  logging ? console.log('createHBarChart: ', id, samples.find(o => o.id == id).id) : null ;
   
   let trace1 = {
     x: samples.find(o => o.id == id).sample_values.slice(0,10).reverse(),
@@ -32,65 +30,70 @@ function createHBarChart(id){
     text: samples.find(o => o.id == id).otu_labels.slice(0,10).reverse()
   };
 
-  let data = [trace1];
   Plotly.newPlot("bar", [trace1]);
 
   return;
 }
 
 function createBubbleChart(id){
-  //logging?console.log('createHBarChart: ', id, samples.find(o => o.id == id).id): '' ;
-  console.log('createBubbleChart: ', id, samples.find(o => o.id == id).id);
+  logging ? console.log('createBubbleChart: ', id, samples.find(o => o.id == id).id) : null ;
   
   let trace1 = {
-    x: samples.find(o => o.id == id).sample_values.slice(0,10).reverse(),
-    y: samples.find(o => o.id == id).otu_ids.slice(0,10).map(n => `OTU ${n}`).reverse(),
-    type: 'bar',
-    orientation: 'h',
-    text: samples.find(o => o.id == id).otu_labels.slice(0,10).reverse()
+    x: samples.find(o => o.id == id).otu_ids,
+    y: samples.find(o => o.id == id).sample_values,
+    mode: 'markers',
+    text: samples.find(o => o.id == id).otu_labels,
+    marker: {color: samples.find(o => o.id == id).otu_ids,
+             size: samples.find(o => o.id == id).sample_values}
   };
 
-  let data = [trace1];
-  Plotly.newPlot("bar", [trace1]);
+  let layout = {xaxis: 
+                 {title:
+                    {text: 'OTU ID'}
+                 }
+               };
+
+  Plotly.newPlot("bubble", [trace1], layout);
 
   return;
 }
 
 async function optionChanged(value){
-  //logging?console.log('optionChanged', value, samples.find(o => o.id == value).id, metadata.find(o => o.id == value).id):null;
-  console.log('optionChanged', value, samples.find(o => o.id == value).id, metadata.find(o => o.id == value).id);
+  logging ? console.log('optionChanged', value, samples.find(o => o.id == value).id, metadata.find(o => o.id == value).id) : null;
   
+  //Create objects in parallel
   await Promise.all([populateDemoInfo(value),
                      createHBarChart(value),
                      createBubbleChart(value)]);
 
-  console.log('optionChanged Complete', value, samples.find(o => o.id == value).id, metadata.find(o => o.id == value).id);
+  logging ? console.log('optionChanged Complete', value, samples.find(o => o.id == value).id, metadata.find(o => o.id == value).id) : null;
 
   return;
 }
  
 // Promise Pending
 const dataPromise = d3.json(url);
-logging?console.log("Data Promise: ", dataPromise):null;
+logging ? console.log("Data Promise: ", dataPromise) : null;
 
 // Fetch the JSON data  
 d3.json(url).then(function(data) {
-  logging?console.log('Data: ', data):null;
+  logging ? console.log('Data: ', data) : null;
   
+  //Initialize web page
+  samples.push(data.samples[0]);
+  metadata.push(data.metadata[0]); 
+  optionChanged(data.samples[0].id);
+
   //Populate select element with json.
   let ele = document.getElementById('selDataset');
   for (i in data.metadata) { 
     ele.innerHTML = ele.innerHTML + '<option value="' + data.samples[i].id + '">' + data.samples[i].id + '</option>';
     
     //Store JSON in an array
-    samples.push(data.samples[i]);
-    metadata.push(data.metadata[i]); 
+    if (i > 0){
+      samples.push(data.samples[i]);
+      metadata.push(data.metadata[i]);
+    } 
   }
-
-    //Initialize page
-    logging?console.log('Value: ', ele.value):null;
-    optionChanged(ele.value);
-
-  }
-);
+});
 
